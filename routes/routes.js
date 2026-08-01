25e02d9f126627744423effb78a2bac89c1c055f
MuhammadWaqar7615 <mwaqar7615@gmail.com>
2026-08-01 23:56:51 +0500

    feat: add POST route for showfeed in routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { showfeed, postsForm } = require('../controllers/controller');
 
 router.get('/create-post', postsForm)
+router.post('/', showfeed)
 router.get('/', showfeed)
 
 module.exports = router;
