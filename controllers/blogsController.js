7652041b4157a9972ea8fef6a9d7b91e5090243f
MuhammadWaqar7615 <mwaqar7615@gmail.com>
2026-08-02 17:00:31 +0500

    feat: add blogsController with CRUD operations for blog management

---
+++ controllers/blogsController.js
@@ -0,0 +1,32 @@
+const Blogs = require("../models/blogsModel");
+const path = require("path");
+
+const getBlogs = async (req, res) => {
+  const blogs = await Blogs.find();
+  console.log("all blogs: ", blogs);
+
+  console.log("getBlogs: ", req.url);
+  res.render("home", { title: "home page", "allBlogs": blogs });
+};
+
+const blogForm = (req, res) => {
+  console.log("blogForm: ", req.url);
+  res.render("createBlogs", { title: "Create Blogs" });
+};
+
+const createBlog = async (req, res) => {
+  console.log("createBlog: ", req.url);
+  const formData = req.body;
+  const data = Object.keys(formData);
+
+  const blogCreation = await Blogs.create(formData);
+  console.log(blogCreation);
+
+  res.redirect(`/`);
+};
+
+module.exports = {
+  getBlogs,
+  blogForm,
+  createBlog,
+};
