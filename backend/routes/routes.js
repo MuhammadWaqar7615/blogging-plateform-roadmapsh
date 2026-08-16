77dd72ed05ccaa1908da6b53936c24e0cecdc181
MuhammadWaqar7615 <mwaqar7615@gmail.com>
2026-08-16 20:16:23 +0500

    fix: add deleteBlog route to handle blog deletion requests

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { getBlogs, blogForm, createBlog } = require('../controllers/blogsController');
+const { getBlogs, blogForm, createBlog, deleteBlog } = require('../controllers/blogsController');
 
-router.get('/blogs', getBlogs)
-router.get('/create-post', blogForm)
+router.get('/blogs', getBlogs);
+router.get('/create-post', blogForm);
 router.post('/', createBlog);
+router.delete('/', deleteBlog);
 
 module.exports = router;
