a66992c41ff23362323f54ddab691052c4a86571
MuhammadWaqar7615 <mwaqar7615@gmail.com>
2026-08-18 19:21:14 +0500

    fix: add updateBlog route to routes for blog updates

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { getBlogs, blogForm, createBlog, deleteBlog } = require('../controllers/blogsController');
+const { getBlogs, blogForm, createBlog, deleteBlog, updateBlog } = require('../controllers/blogsController');
 
 router.get('/blogs', getBlogs);
 router.get('/create-post', blogForm);
 router.post('/', createBlog);
 router.delete('/', deleteBlog);
+router.patch('/', updateBlog)
 
 module.exports = router;
